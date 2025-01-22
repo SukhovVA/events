ced1763d2d984d1ad0d23546d876aba1389152c1
Sergey Komarov <sk-2006@mail.ru>
2025-01-22 18:05:31 +0300

    update migration

--- migrations/Version20250122150433.php
+++ migrations/Version20250122150433.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250121111250 extends AbstractMigration
+final class Version20250122150433 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,17 @@ final class Version20250121111250 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE event (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, starts_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, ends_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, academic_hours DOUBLE PRECISION DEFAULT NULL, remote_link VARCHAR(255) DEFAULT NULL, slug VARCHAR(255) NOT NULL, active BOOLEAN DEFAULT true NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE event (id SERIAL NOT NULL, cover_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, starts_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, ends_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, academic_hours DOUBLE PRECISION DEFAULT NULL, remote_link VARCHAR(255) DEFAULT NULL, slug VARCHAR(255) NOT NULL, active BOOLEAN DEFAULT true NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_3BAE0AA7989D9B62 ON event (slug)');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA7922726E9 ON event (cover_id)');
         $this->addSql('CREATE TABLE media_link (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, original_name VARCHAR(255) NOT NULL, hash VARCHAR(255) NOT NULL, type SMALLINT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7922726E9 FOREIGN KEY (cover_id) REFERENCES media_link (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE event DROP CONSTRAINT FK_3BAE0AA7922726E9');
         $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE media_link');
     }
