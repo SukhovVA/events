219aea069ef4975eb61bad7a3bfb3ac3449973bb
Sergey Komarov <sk-2006@mail.ru>
2025-01-23 15:38:50 +0300

    added redis

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,16 +12,9 @@ class Controller extends AbstractController
     #[Route('health', name: 'health', methods: ['GET'])]
     public function health(CacheInterface $cache): JsonResponse
     {
-        $data = $cache->get('health', function (CacheItemInterface $item) {
-            $item->expiresAfter(3600);
-
-            return ['health' => true];
-        });
-
         return $this->json([
             'success' => true,
-            'status' => 'OK',
-            'data' => $data
+            'status' => 'OK'
         ]);
     }
 }
